5167c85db71d57de3ced704538e1fca969371fd6
finemflame <84194780+finemflame@users.noreply.github.com>
2023-05-06 12:52:11 +0200

    Update [slug].js

--- src/pages/posts/[slug].js
+++ src/pages/posts/[slug].js
@@ -1,4 +1,4 @@
-import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { Helmet } from 'react-helmet';
 
 import { getPostBySlug, getRecentPosts, getRelatedPosts, postPathBySlug } from 'lib/posts';
@@ -118,81 +118,105 @@ export default function Post({ post, socialImage, related }) {
                 <span>
                   More from <Link href={relatedPostsTitle.link}>{relatedPostsTitle.name}</Link>
                 </span>
-              ) : (
-                <span>More Posts</span>
-              )}
-              <ul>
-                {relatedPostsList.map((post) => (
-                  <li key={post.title}>
-                    <Link href={postPathBySlug(post.slug)}>{post.title}</Link>
-                  </li>
-                ))}
-              </ul>
-            </div>
+             
+          ) : (
+            <span>More Posts</span>
           )}
-        </Container>
-      </Section>
-    </Layout>
-  );
+          <ul>
+            {relatedPostsList.map((post) => (
+              <li key={post.title}>
+                <Link href={postPathBySlug(post.slug)}>{post.title}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </Container>
+  </Section>
+</Layout>
+);
 }
 
 export async function getStaticProps({ params = {} } = {}) {
-  const { post } = await getPostBySlug(params?.slug);
+const { post } = await getPostBySlug(params?.slug);
 
-  if (!post) {
-    return {
-      props: {},
-      notFound: true,
-    };
-  }
+if (!post) {
+return {
+props: {},
+notFound: true,
+};
+}
 
-  const { categories, databaseId: postId } = post;
+const { categories, databaseId: postId } = post;
 
-  const props = {
-    post,
-    socialImage: `${process.env.OG_IMAGE_DIRECTORY}/${params?.slug}.png`,
-  };
+const props = {
+post,
+socialImage: ${process.env.OG_IMAGE_DIRECTORY}/${params?.slug}.png,
+};
 
-  const { category: relatedCategory, posts: relatedPosts } = (await getRelatedPosts(categories, postId)) || {};
-  const hasRelated = relatedCategory && Array.isArray(relatedPosts) && relatedPosts.length;
+const { category: relatedCategory, posts: relatedPosts } = (await getRelatedPosts(categories, postId)) || {};
+const hasRelated = relatedCategory && Array.isArray(relatedPosts) && relatedPosts.length;
 
-  if (hasRelated) {
-    props.related = {
-      posts: relatedPosts,
-      title: {
-        name: relatedCategory.name || null,
-        link: categoryPathBySlug(relatedCategory.slug),
-      },
-    };
-  }
+if (hasRelated) {
+props.related = {
+posts: relatedPosts,
+title: {
+name: relatedCategory.name || null,
+link: categoryPathBySlug(relatedCategory.slug),
+},
+};
+}
 
-  return {
-    props,
-  };
+return {
+props,
+};
 }
 
 export async function getStaticPaths() {
-  // Only render the most recent posts to avoid spending unecessary time
-  // querying every single post from WordPress
+// Only render the most recent posts to avoid spending unnecessary time
+// querying every single post from WordPress
 
-  // Tip: this can be customized to use data or analytitcs to determine the
-  // most popular posts and render those instead
+// Tip: this can be customized to use data or analytics to determine the
+// most popular posts and render those instead
 
-  const { posts } = await getRecentPosts({
-    count: process.env.POSTS_PRERENDER_COUNT, // Update this value in next.config.js!
-    queryIncludes: 'index',
-  });
+const { posts } = await getRecentPosts({
+count: process.env.POSTS_PRERENDER_COUNT, // Update this value in next.config.js!
+queryIncludes: 'index',
+});
 
-  const paths = posts
-    .filter(({ slug }) => typeof slug === 'string')
-    .map(({ slug }) => ({
-      params: {
-        slug,
-      },
-    }));
+const paths = posts
+.filter(({ slug }) => typeof slug === 'string')
+.map(({ slug }) => ({
+params: {
+slug,
+},
+}));
 
-  return {
-    paths,
-    fallback: 'blocking',
-  };
+return {
+paths,
+fallback: 'blocking',
+};
+}
+
+// Redirect URLs with Vercel domain to the custom domain
+export async function getServerSideProps(context) {
+const { req, params } = context;
+const { headers } = req;
+
+const url = new URL(https://${headers.host}${req.url});
+
+// If the URL doesn't contain the Vercel subdomain, return null
+if (!url.host.startsWith('babahehekujbes.vercel.app')) {
+return { props: {} };
+}
+
+const { slug } = params;
+
+// Redirect to the custom domain using a 301 (permanent) redirect
+return {
+redirect: {
+destination: https://dailytrending.info/${slug},
+permanent: true,
+},
+};
 }
