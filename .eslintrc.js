15c975333cf0b00e83c07fd8e5fd9daa99c68595
imrookhan44 <91519591+imrookhan44@users.noreply.github.com>
2023-05-09 20:17:36 +0500

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,5 +30,8 @@ module.exports = {
     'react/prop-types': 'off',
     'react/react-in-jsx-scope': 'off',
     '@next/next/no-img-element': 'off',
+      'prettier/prettier': 'off',
+    //ignore the defined but never used error
+    'no-unused-vars': 'off'
   },
 };
