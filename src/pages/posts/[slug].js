ff1503d77d9acf7266bd4713a63cf94002ab2c94
imrookhan44 <khannihar921@gmail.com>
2023-05-10 13:38:15 +0500

    fix the slug.js code

--- src/pages/posts/[slug].js
+++ src/pages/posts/[slug].js
@@ -1,3 +1,220 @@
+// import Link from 'next/link';
+// import { Helmet } from 'react-helmet';
+
+// import { getPostBySlug, getRecentPosts, getRelatedPosts, postPathBySlug } from 'lib/posts';
+// import { categoryPathBySlug } from 'lib/categories';
+// import { formatDate } from 'lib/datetime';
+// import { ArticleJsonLd } from 'lib/json-ld';
+// import { helmetSettingsFromMetadata } from 'lib/site';
+// import useSite from 'hooks/use-site';
+// import usePageMetadata from 'hooks/use-page-metadata';
+
+// import Layout from 'components/Layout';
+// import Header from 'components/Header';
+// import Section from 'components/Section';
+// import Container from 'components/Container';
+// import Content from 'components/Content';
+// import Metadata from 'components/Metadata';
+// import FeaturedImage from 'components/FeaturedImage';
+
+// import styles from 'styles/pages/Post.module.scss';
+// import { getRedirectStatus } from 'lib/redirect';
+// import getConfig from 'next/config';
+
+// const { publicRuntimeConfig } = getConfig();
+// const { WORDPRESS_DOMAIN } = publicRuntimeConfig;
+
+// export default function Post({ post, socialImage, related }) {
+//   const {
+//     title,
+//     metaTitle,
+//     description,
+//     content,
+//     date,
+//     author,
+//     categories,
+//     modified,
+//     featuredImage,
+//     isSticky = false,
+//   } = post;
+
+//   const { metadata: siteMetadata = {}, homepage } = useSite();
+
+//   if (!post.og) {
+//     post.og = {};
+//   };
+
+//   post.og.imageUrl = `${homepage}${socialImage}`;
+//   post.og.imageSecureUrl = post.og.imageUrl;
+//   post.og.imageWidth = 2000;
+//   post.og.imageHeight = 1000;
+
+//   const { metadata } = usePageMetadata({
+//     metadata: {
+//       ...post,
+//       title: metaTitle,
+//       description: description || post.og?.description || `Read more about ${title}`,
+//     },
+//   });
+
+//   if (process.env.WORDPRESS_PLUGIN_SEO !== true) {
+//     metadata.title = `${title} - ${siteMetadata.title}`;
+//     metadata.og.title = metadata.title;
+//     metadata.twitter.title = metadata.title;
+//   }
+
+//   const metadataOptions = {
+//     compactCategories: false,
+//   };
+
+//   const { posts: relatedPostsList, title: relatedPostsTitle } = related || {};
+
+//   const helmetSettings = helmetSettingsFromMetadata(metadata);
+
+//   return (
+//     <Layout>
+//       <Helmet {...helmetSettings} />
+//       <ArticleJsonLd post={post} siteTitle={siteMetadata.title} />
+
+//       <Header>
+//         {featuredImage && (
+//           <FeaturedImage
+//             {...featuredImage}
+//             src={featuredImage.sourceUrl}
+//             dangerouslySetInnerHTML={featuredImage.caption}
+//           />
+//         )}
+//         <h1
+//           className={styles.title}
+//           dangerouslySetInnerHTML={{
+//             __html: title,
+//           }}
+//         />
+//         <Metadata
+//           className={styles.postMetadata}
+//           date={date}
+//           author={author}
+//           categories={categories}
+//           options={metadataOptions}
+//           isSticky={isSticky}
+//         />
+//       </Header>
+
+//       <Content>
+//         <Section>
+//           <Container>
+//             <div
+//               className={styles.content}
+//               dangerouslySetInnerHTML={{
+//                 __html: content,
+//               }}
+//             />
+//           </Container>
+//         </Section>
+//       </Content>
+
+//       <Section className={styles.postFooter}>
+//         <Container>
+//           <p className={styles.postModified}>Last updated on {formatDate(modified)}.</p>
+//           {Array.isArray(relatedPostsList) && relatedPostsList.length > 0 && (
+//             <div className={styles.relatedPosts}>
+//               {relatedPostsTitle.name ? (
+//                 <span>
+//                   More from <Link href={relatedPostsTitle.link}>{relatedPostsTitle.name}</Link>
+//                 </span>
+//               ) : (
+//                 <span>More Posts</span>
+//               )}
+//               <ul>
+//                 {relatedPostsList.map((post) => (
+//                   <li key={post.title}>
+//                     <Link href={postPathBySlug(post.slug)}>{post.title}</Link>
+//                   </li>
+//                 ))}
+//               </ul>
+//             </div>
+//           )}
+//         </Container>
+//       </Section>
+//     </Layout>
+//   );
+// };
+
+// export async function getStaticProps({ params = {}, req }) {
+//   const { post } = await getPostBySlug(params?.slug);
+
+//   if (!post) {
+//     return {
+//       props: {},
+//       notFound: true,
+//     }
+//   };
+//   // Redirect to WordPress domain
+//   console.log('WORDPRESS_DOMAIN', WORDPRESS_DOMAIN);
+//   const redirectUrl = (req, WORDPRESS_DOMAIN);
+//   const redirectStatus = getRedirectStatus(req);
+//   if (redirectUrl) {
+//     return {
+//       redirect: {
+//         destination: redirectUrl,
+//         permanent: redirectStatus === 301,
+//       },
+//     };
+//   };
+
+
+//   const { categories, databaseId: postId } = post;
+
+//   const props = {
+//     post,
+//     socialImage: `${process.env.OG_IMAGE_DIRECTORY}/${params?.slug}.png`
+//   };
+
+//   const { category: relatedCategory, posts: relatedPosts } = (await getRelatedPosts(categories, postId)) || {};
+//   const hasRelated = relatedCategory && Array.isArray(relatedPosts) && relatedPosts.length;
+
+//   if (hasRelated) {
+//     props.related = {
+//       posts: relatedPosts,
+//       title: {
+//         name: relatedCategory.name || null,
+//         link: categoryPathBySlug(relatedCategory.slug),
+//       },
+//     };
+//   }
+
+//   return {
+//     props,
+//   };
+// };
+
+// export async function getStaticPaths() {
+//   // Only render the most recent posts to avoid spending unecessary time
+//   // querying every single post from WordPress
+
+//   // Tip: this can be customized to use data or analytitcs to determine the
+//   // most popular posts and render those instead
+
+//   const { posts } = await getRecentPosts({
+//     count: process.env.POSTS_PRERENDER_COUNT, // Update this value in next.config.js!
+//     queryIncludes: 'index',
+//   });
+
+//   const paths = posts
+//     .filter(({ slug }) => typeof slug === 'string')
+//     .map(({ slug }) => ({
+//       params: {
+//         slug,
+//       },
+//     }));
+
+//   return {
+//     paths,
+//     fallback: 'blocking',
+//   };
+// };
+
+
 import Link from 'next/link';
 import { Helmet } from 'react-helmet';
 
@@ -18,11 +235,6 @@ import Metadata from 'components/Metadata';
 import FeaturedImage from 'components/FeaturedImage';
 
 import styles from 'styles/pages/Post.module.scss';
-import { getRedirectStatus } from 'lib/redirect';
-import getConfig from 'next/config';
-
-const { publicRuntimeConfig } = getConfig();
-const { WORDPRESS_DOMAIN } = publicRuntimeConfig;
 
 export default function Post({ post, socialImage, related }) {
   const {
@@ -42,7 +254,7 @@ export default function Post({ post, socialImage, related }) {
 
   if (!post.og) {
     post.og = {};
-  };
+  }
 
   post.og.imageUrl = `${homepage}${socialImage}`;
   post.og.imageSecureUrl = post.og.imageUrl;
@@ -74,6 +286,7 @@ export default function Post({ post, socialImage, related }) {
   return (
     <Layout>
       <Helmet {...helmetSettings} />
+
       <ArticleJsonLd post={post} siteTitle={siteMetadata.title} />
 
       <Header>
@@ -138,36 +351,23 @@ export default function Post({ post, socialImage, related }) {
       </Section>
     </Layout>
   );
-};
+}
 
-export async function getStaticProps({ params = {}, req }) {
+export async function getStaticProps({ params = {} } = {}) {
   const { post } = await getPostBySlug(params?.slug);
 
   if (!post) {
     return {
       props: {},
       notFound: true,
-    }
-  };
-  // Redirect to WordPress domain
-  console.log('WORDPRESS_DOMAIN', WORDPRESS_DOMAIN);
-  const redirectUrl = (req, WORDPRESS_DOMAIN);
-  const redirectStatus = getRedirectStatus(req);
-  if (redirectUrl) {
-    return {
-      redirect: {
-        destination: redirectUrl,
-        permanent: redirectStatus === 301,
-      },
     };
-  };
-
+  }
 
   const { categories, databaseId: postId } = post;
 
   const props = {
     post,
-    socialImage: `${process.env.OG_IMAGE_DIRECTORY}/${params?.slug}.png`
+    socialImage: `${process.env.OG_IMAGE_DIRECTORY}/${params?.slug}.png`,
   };
 
   const { category: relatedCategory, posts: relatedPosts } = (await getRelatedPosts(categories, postId)) || {};
@@ -186,7 +386,7 @@ export async function getStaticProps({ params = {}, req }) {
   return {
     props,
   };
-};
+}
 
 export async function getStaticPaths() {
   // Only render the most recent posts to avoid spending unecessary time
@@ -212,4 +412,4 @@ export async function getStaticPaths() {
     paths,
     fallback: 'blocking',
   };
-};
+}
